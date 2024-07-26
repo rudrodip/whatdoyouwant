7e27bc7798cd33c7103143dbbe4bc900e625684c
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-26 16:52:45 +0600

    init site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "What do you want? 👀",
+  description: "tell me what do you want? 👀",
+  url: "https://whatyouwant.rdsx.dev",
+  keywords: [
+    "meme",
+    "cat meme",
+    "image generation"
+  ],
+  ogImage: "https://whatyouwant.rdsx.dev/og.png",
+  links: {
+    twitter: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/whatyouwant"
+  }
+};
