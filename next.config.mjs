644514e6d5120b6381aa44f6afaa1415bd36ede9
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-28 10:32:47 +0600

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,7 @@ const nextConfig = {
   },
   experimental: {
     outputFileTracingIncludes: {
-      '/src/actions': ['./pulic/**/*'],
-      '/src/lib': ['./pulic/**/*'],
+      '/': ['./pulic/**/*'],
     },
   },
 };
