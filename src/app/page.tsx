2309fdd9034d0db3b191ad4155c931bd7a2f1891
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-30 10:48:21 +0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import MainForm from "@/components/main-form";
 import TotalRequests from "@/components/total-requests";
 
-export const config = {
-  maxDuration: 60,
-};
+export const maxDuration = 60;
 
 export default function Home() {
   return (
