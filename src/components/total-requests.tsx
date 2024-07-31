88b85921193988e1ef91690a85555a45365c1ef3
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-31 10:40:30 +0600

    replaced client component with server component

--- src/components/total-requests.tsx
+++ src/components/total-requests.tsx
@@ -1,46 +1,14 @@
-"use client";
+import { getTotalRequests } from "@/lib/firebase";
 
-import { getTotalRequests } from '@/lib/firebase';
-import React, { useState, useEffect } from 'react';
+export const revalidate = 0;
 
-const TotalRequests = () => {
-  const [totalRequests, setTotalRequests] = useState(0);
-  const [displayCount, setDisplayCount] = useState(0);
-
-  useEffect(() => {
-    const fetchTotalRequests = async () => {
-      try {
-        const requests = await getTotalRequests();
-        setTotalRequests(requests);
-      } catch (error) {
-        console.error("Failed to fetch total requests:", error);
-      }
-    };
-
-    fetchTotalRequests();
-  }, []);
-
-  useEffect(() => {
-    if (displayCount < totalRequests) {
-      const animationDuration = 1000;
-      const framesPerSecond = 120;
-      const increment = (totalRequests - displayCount) / (animationDuration / 1000 * framesPerSecond);
-
-      const intervalId = setInterval(() => {
-        setDisplayCount(prevCount => {
-          const nextCount = prevCount + increment;
-          return nextCount >= totalRequests ? totalRequests : nextCount;
-        });
-      }, 1000 / framesPerSecond);
-
-      return () => clearInterval(intervalId);
-    }
-  }, [totalRequests, displayCount]);
+const TotalRequests = async () => {
+  const totalRequests = await getTotalRequests();
 
   return (
     <div className="absolute bottom-5 left-5">
       <p className="text-sm text-muted-foreground transition-all duration-300 ease-in-out">
-        {Math.round(displayCount).toLocaleString()}
+        {totalRequests}
       </p>
     </div>
   );
