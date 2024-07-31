e15f6642cf7f348a418befe4d831df831982a38d
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-31 11:39:12 +0600

    dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import MainForm from "@/components/main-form";
 import TotalRequests from "@/components/total-requests";
 
 export const maxDuration = 60;
+export const dynamic = 'force-dynamic'
 
 export default function Home() {
   return (
