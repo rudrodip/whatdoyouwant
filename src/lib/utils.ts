73f3d11d1e71e505582280ab4bb2f25aaa2fcd90
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-10 14:15:09 +0600

    added base path

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getBasePath() {
+  return process.env.NODE_ENV === "production"
+    ? `https://${process.env.NEXT_PUBLIC_URL}`
+    : "http://localhost:3000";
 }
