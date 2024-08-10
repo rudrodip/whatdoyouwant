dffb21dd5ca98209f34feecb358407015bd4a297
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-10 15:07:05 +0600

    added more buttons

--- src/components/main-form.tsx
+++ src/components/main-form.tsx
@@ -17,18 +17,25 @@ import { Input } from "@/components/ui/input";
 import {
   ArrowRightIcon,
   DownloadIcon,
+  Share1Icon,
+  ClipboardCopyIcon,
+  ReloadIcon,
 } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { cn } from "@/lib/utils";
+import { cn, getBasePath } from "@/lib/utils";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function MainForm() {
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [blob, setBlob] = useState<Blob | null>(null);
   const [memeUrl, setMemeUrl] = useState<string>("");
   const searchParam = useSearchParams();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -43,11 +50,14 @@ export default function MainForm() {
     const ref = searchParam.get("ref") ?? undefined;
 
     try {
-      const imageResponse = await fetch("/api/og?query=" + values.query + "&ref=" + ref);
+      const imageResponse = await fetch(
+        "/api/og?query=" + values.query + "&ref=" + ref
+      );
       const imageBlob = await imageResponse.blob();
 
       if (imageBlob) {
         const imageUrl = URL.createObjectURL(imageBlob);
+        setBlob(imageBlob);
         setMemeUrl(imageUrl);
         setError(false);
       } else {
@@ -66,7 +76,45 @@ export default function MainForm() {
 
     form.setValue("query", query);
     onSubmit({ query });
-  }, [form, searchParam])
+  }, [form, searchParam]);
+
+  const handleCopy = async () => {
+    if (!blob) return;
+    try {
+      navigator.clipboard.write([
+        new ClipboardItem({
+          "image/png": blob,
+        }),
+      ]);
+      toast.success("Copied image to clipboard");
+    } catch (error) {
+      toast.error("Failed to copy");
+      console.error("Failed to copy: ", error);
+    }
+  };
+
+  const handleCopyUrl = async () => {
+    try {
+      navigator.clipboard.writeText(
+        `${getBasePath()}/?query=${form.getValues("query")}`
+      );
+      toast.success("Copied to clipboard");
+    } catch (error) {
+      toast.error("Failed to copy");
+      console.error("Failed to copy: ", error);
+    }
+  };
+
+  const resetState = () => {
+    setMemeUrl("");
+    setBlob(null);
+    setSubmitted(false);
+    form.setValue("query", "");
+    const query = searchParam.get("query");
+    if (query) {
+      router.push("/");
+    }
+  }
 
   return (
     <main className="w-full flex justify-center items-center p-2 lg:p-0">
@@ -103,21 +151,73 @@ export default function MainForm() {
           </form>
         </Form>
       )}
-      
-      <Image src="/loading.gif" alt="loading" width={300} height={300} unoptimized className={cn(!loading && "hidden")} />
-      <Image src="/error.gif" alt="loading" width={300} height={300} unoptimized className={cn(!error && "hidden")} />
+
+      <Image
+        src="/loading.gif"
+        alt="loading"
+        width={300}
+        height={300}
+        unoptimized
+        className={cn(!loading && "hidden")}
+      />
+      <Image
+        src="/error.gif"
+        alt="loading"
+        width={300}
+        height={300}
+        unoptimized
+        className={cn(!error && "hidden")}
+      />
       {memeUrl && (
         <div className="relative w-full max-w-lg flex flex-col">
-          <Image src={memeUrl} alt="loading" width={1000} height={700} unoptimized />
-          <Button
-            variant="ghost"
-            className="size-8 p-0 absolute bottom-0 right-0"
-            asChild
-          >
-            <a href={memeUrl} download={form.getValues("query")}>
-              <DownloadIcon />
-            </a>
-          </Button>
+          <Image
+            src={memeUrl}
+            alt="loading"
+            width={1000}
+            height={700}
+            unoptimized
+          />
+          <div className="absolute bottom-0 right-0 flex items-center">
+            <Button
+              variant="ghost"
+              className="size-8 p-0 cursor-pointer"
+              asChild
+              onClick={resetState}
+            >
+              <div>
+                <ReloadIcon />
+              </div>
+            </Button>
+            <Button
+              variant="ghost"
+              className="size-8 p-0 cursor-pointer"
+              asChild
+              onClick={handleCopyUrl}
+            >
+              <div>
+                <Share1Icon />
+              </div>
+            </Button>
+            <Button
+              variant="ghost"
+              className="size-8 p-0 cursor-pointer"
+              asChild
+              onClick={handleCopy}
+            >
+              <div>
+                <ClipboardCopyIcon />
+              </div>
+            </Button>
+            <Button
+              variant="ghost"
+              className="size-8 p-0 cursor-pointer"
+              asChild
+            >
+              <a href={memeUrl} download={form.getValues("query")}>
+                <DownloadIcon />
+              </a>
+            </Button>
+          </div>
         </div>
       )}
     </main>
