f1ef48a971cf734b0baf4371310faedb43b52221
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-10 14:15:14 +0600

    dynamic og

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 import Socials from "@/components/socials";
 import MainForm from "@/components/main-form";
 import TotalRequests from "@/components/total-requests";
+import { Metadata, ResolvingMetadata } from "next";
+import { getBasePath } from "@/lib/utils";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const query = searchParams.query;
+  const base = getBasePath();
+  const ogImage = query
+    ? `${base}/api/og/?query=${encodeURIComponent(query as string)}`
+    : `${base}/og.png`;
+
+  return {
+    openGraph: {
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+  };
+}
 
 export const maxDuration = 60;
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
