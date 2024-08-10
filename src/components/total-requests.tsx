2e4817efdf95c3229ff3e375bc1807764f3dc6c4
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-10 13:37:58 +0600

    updated

--- src/components/total-requests.tsx
+++ src/components/total-requests.tsx
@@ -1,10 +1,14 @@
-import { getTotalRequests } from "@/lib/firebase";
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
+})
 
 export const dynamic = 'force-dynamic'
 
 const TotalRequests = async () => {
-  const totalRequests = await getTotalRequests();
-
+  const totalRequests = await redis.get('total_requests') as string;
   return (
     <div className="absolute bottom-5 left-5">
       <p className="text-sm text-muted-foreground transition-all duration-300 ease-in-out">
